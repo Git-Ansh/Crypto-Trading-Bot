893551db058f511f16724505af18a343fc13d14e
Ansh Shah <anshjarvis2003@gmail.com>
2025-01-11 13:25:39 -0500

    Add server address

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://crypto-trading-bot-sa5d.onrender.com/api',
   withCredentials: true, // Send cookies with requests
 });
 
