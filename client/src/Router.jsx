62727f47cd1cf49e2b25b40028a54777663427d4
Ansh Shah <anshjarvis2003@gmail.com>
2025-01-14 23:12:35 -0500

    Refactor route structure in AppRouter for improved readability

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -28,20 +28,19 @@ const AppRouter = () => {
       {/* A protected wrapper route for everything else */}
       <Route element={<ProtectedRoute />}>
         {/* These routes are only accessible if isAuthenticated === true */}
-        <Route path="/" element={<App />}>
-          <Route path="/dashboard/:userId" element={<Dashboard />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/invoices" element={<Invoices />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/bar" element={<Bar />} />
-          <Route path="/pie" element={<Pie />} />
-          <Route path="/stream" element={<Stream />} />
-          <Route path="/line" element={<Line />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/geography" element={<Geography />} />
-        </Route>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard/:userId" element={<Dashboard />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/bar" element={<Bar />} />
+        <Route path="/pie" element={<Pie />} />
+        <Route path="/stream" element={<Stream />} />
+        <Route path="/line" element={<Line />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/geography" element={<Geography />} />
       </Route>
     </Routes>
   );
